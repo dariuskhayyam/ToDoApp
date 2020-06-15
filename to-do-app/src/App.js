3796675787d0a368caf09e989124b6788bc5b167
darius khayyam <dv.Khayyam@gmail.com>
2020-06-15 13:16:23 +0200

    actually started the app

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,9 +1,10 @@
 import React from "react"
+import CheckList from "./components/checkList"
 
 function App(){
     return(
         
-        <h1>Hello World!</h1>
+        <CheckList />
         
     )
 }
---
+++ to-do-app/src/components/checkList.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function CheckList(){
+    return(
+        <div>
+
+            <input type = "checkbox"></input> Give Shali all your love
+            <br/>
+            <input type = "checkbox"></input> Make sure Shali is feeling good
+            <br/>
+            <input type = "checkbox"></input> Eat that pussy all day long 🤤
+
+        </div>
+    )
+}
+
+export default CheckList
